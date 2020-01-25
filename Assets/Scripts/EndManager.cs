984b01450e726e958e1486f83d2aa3390b12265c
Sujoy Ghosh <sujoyself@gmail.com>
2020-01-25 16:09:26 +0530

    Added simple game menu, game end script and some fixes.

---
+++ Assets/Scripts/EndManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement ;
+
+public class EndManager : MonoBehaviour
+{
+    public GameObject hero ;
+    public Transform endStart ;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject == hero)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex) ;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject gameMenu ;
+    
+    void Start()
+    {
+        if (gameMenu.activeSelf)
+            Time.timeScale = 0f ;
+    }
+
+    public void Play()
+    {
+        gameMenu.SetActive(false) ;
+        Time.timeScale = 1f ;
+    }
+}
